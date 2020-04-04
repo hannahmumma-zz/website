e29ec124d1d27362899245e68a91907929d720ff
Hannah Mumma <hannahmumma@gmail.com>
2020-04-04 17:37:38 -0400

    lint button

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -4,41 +4,41 @@ import NavButtonBars from "../NavButtonBars";
 import styled, { ThemeProvider } from "styled-components";
 
 class Button extends React.Component {
-	render() {
-		const OpenMenu = styled.button`
-			background: transparent;
-			border: none;
-			cursor: pointer;
-			display: flex;
-			flex-direction: column;
-			grid-column: 3;
-			padding: 5px;
-			@media (min-width: 768px) {
-				display: none;
-			}
-			&[aria-expanded="true"] span {
-				&:first-of-type {
-					transform: rotate(-45deg) translate(-2px, 3px);
-				}
-				&:nth-of-type(2) {
-					opacity: 0;
-				}
-				&:last-of-type {
-					transform: rotate(45deg) translate(-4px, -5px);
-				}
-			}
-		`;
+  render() {
+    const OpenMenu = styled.button`
+      background: transparent;
+      border: none;
+      cursor: pointer;
+      display: flex;
+      flex-direction: column;
+      grid-column: 3;
+      padding: 5px;
+      @media (min-width: 768px) {
+        display: none;
+      }
+      &[aria-expanded="true"] span {
+        &:first-of-type {
+          transform: rotate(-45deg) translate(-2px, 3px);
+        }
+        &:nth-of-type(2) {
+          opacity: 0;
+        }
+        &:last-of-type {
+          transform: rotate(45deg) translate(-4px, -5px);
+        }
+      }
+    `;
 
-		return (
-			<OpenMenu
-				className="btn"
-				onClick={this.props.navButtonClick}
-				aria-expanded={this.props.expanded}
-			>
-				{this.props.name !== "navButton" ? this.props.name : <NavButtonBars />}
-			</OpenMenu>
-		);
-	}
+    return (
+      <OpenMenu
+        className="btn"
+        onClick={this.props.navButtonClick}
+        aria-expanded={this.props.expanded}
+      >
+        {this.props.name !== "navButton" ? this.props.name : <NavButtonBars />}
+      </OpenMenu>
+    );
+  }
 }
 
 export default Button;
